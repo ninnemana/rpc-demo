60e8c9c24e0cbfa02c556335798c9adf709364cf
Alex Ninneman <alex@ninneman.org>
2019-10-04 14:09:18 -0500

    adds main for testing

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ninnemana/rpc-demo/pkg/vinyltappb"
+)
+
+func main() {
+	a := vinyltappb.Album{
+		Id:          1,
+		Artist:      "Nirvana",
+		Title:       "Nevermind",
+		ReleaseDate: time.Date(1991, time.September, 24, 0, 0, 0, 0, time.UTC).Unix(),
+		Songs: []string{
+			"Smells Like Teen Spirit",
+			"In Bloom",
+			"Come as You Are",
+			"Breed",
+			"Lithium",
+			"Polly",
+			"Territorial Pissings",
+			"Drain You",
+			"Lounge Act",
+			"Stay Away",
+			"On a Plain",
+			"Something in the Way",
+			"Endless, Nameless",
+		},
+	}
+	fmt.Printf("%+v\n", a)
+}
