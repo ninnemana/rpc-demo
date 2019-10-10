968f90d16c40fd2fc05f959332961f51e6dd20e8
Alex Ninneman <alex@ninneman.org>
2019-10-10 12:46:06 -0500

    puts back service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,33 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"context"
+	"log"
+	"sync"
+
+	"github.com/ninnemana/drudge"
+	"github.com/ninnemana/drudge/telemetry"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
 
 	"github.com/ninnemana/rpc-demo/pkg/vinyltappb"
 )
 
-func main() {
-	a := vinyltappb.Album{
-		Id:          1,
-		Artist:      "Nirvana",
-		Title:       "Nevermind",
-		ReleaseDate: time.Date(1991, time.September, 24, 0, 0, 0, 0, time.UTC).Unix(),
-		Songs: []string{
-			"Smells Like Teen Spirit",
-			"In Bloom",
-			"Come as You Are",
-			"Breed",
-			"Lithium",
-			"Polly",
-			"Territorial Pissings",
-			"Drain You",
-			"Lounge Act",
-			"Stay Away",
-			"On a Plain",
-			"Something in the Way",
-			"Endless, Nameless",
+const (
+	tcpAddr = ":8080"
+	rpcAddr = ":8081"
+)
+
+var (
+	options = drudge.Options{
+		BasePath: "/",
+		Addr:     tcpAddr,
+		RPC: drudge.Endpoint{
+			Network: "tcp",
+			Addr:    rpcAddr,
+		},
+		OnRegister:    Register,
+		TraceExporter: telemetry.Jaeger,
+		TraceConfig: telemetry.JaegerConfig{
+			ServiceName: "rpc-demo",
 		},
 	}
-	fmt.Printf("%+v\n", a)
+)
+
+func main() {
+	if err := drudge.Run(context.Background(), options); err != nil {
+		log.Fatalf("Fell out of serving application: %+v", err)
+	}
+}
+
+type Service struct {
+	albums map[int32]*vinyltappb.Album
+	sync.RWMutex
+}
+
+func Register(server *grpc.Server) error {
+	vinyltappb.RegisterTapServer(server, &Service{
+		albums: map[int32]*vinyltappb.Album{},
+	})
+	return nil
+}
+
+func (s *Service) GetAlbum(a *vinyltappb.Album, srv vinyltappb.Tap_GetAlbumServer) error {
+	for k := range s.albums {
+		if k != a.GetId() {
+			continue
+		}
+
+		if err := srv.Send(s.albums[k]); err != nil {
+			return errors.WithMessage(err, "failed to send album over TCP connection")
+		}
+	}
+
+	return nil
+}
+
+func (s *Service) Set(ctx context.Context, a *vinyltappb.Album) (*vinyltappb.Album, error) {
+	if _, ok := s.albums[a.GetId()]; ok {
+		return nil, errors.Errorf("provided album '%d' exists", a.GetId())
+	}
+	s.Lock()
+	s.albums[a.GetId()] = a
+	s.Unlock()
+
+	return s.albums[a.GetId()], nil
 }
